f0a7fde6d9ad7b8fff19adeff80c716159417953
InnoSan <reau_beua@gmail.com>
2023-04-17 12:00:59 +0300

    fixed tech tag hover effect

--- src/lib/components/TechTag.svelte
+++ src/lib/components/TechTag.svelte
@@ -1,20 +1,32 @@
 <script>
     export let title, icon;
 
-	import {fly} from 'svelte/transition';
+	import {fly, slide} from 'svelte/transition';
 
 	let visible = false;
-
-    function toggleVisibility() {
-        visible = !visible;
-    }
 </script>
 
-<div class="flex flex-row items-center gap-2 bg-black bg-opacity-70 backdrop-blur-sm p-3 rounded-2xl icon_container" on:mouseenter={toggleVisibility} on:mouseleave={toggleVisibility}>
+<div
+	class="flex flex-row items-center gap-2 bg-black bg-opacity-70 backdrop-blur-sm p-3 rounded-2xl icon_container"
+	on:mouseenter={() => {visible = true}}
+	on:mouseleave={() => visible = false}>
     <div>
         {@html icon}
     </div>
-    {#if visible}
-        <p transition:fly={{ x: -50, duration: 500 }}>{title}</p>
-    {/if}
+	{#if visible}
+		<div class="title-wrapper"
+			 in:slide="{{axis: 'x', duration: 200}}"
+			 out:slide="{{axis: 'x', duration: 200, delay: 150}}"
+		>
+			<div class="title"
+				 in:fly="{{ x: -50, duration: 200, delay: 150 }}"
+				 out:fly="{{ x: -50, duration: 200}}"
+			>
+				{title}
+			</div>
+		</div>
+	{/if}
 </div>
+
+<style>
+</style>
